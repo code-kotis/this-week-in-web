6067060a42679f0990e11c13a92b9c90a79bf637
Shidhin <shidhincr@gmail.com>
2020-01-09 11:05:27 +0530

    fix: group posts by year

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import get from 'lodash/get'
 import groupBy from 'lodash/groupBy'
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import { getMonthName } from '../utils/date'
 
 import Layout from '../components/layout'
@@ -10,18 +10,68 @@ import SEO from '../components/seo'
 import ContentLeft from '../components/contentLeft'
 import ContentRight from '../components/contentRight'
 
-const getGroupedPosts = posts => {
+const getGroupedPosts = ( posts, type /*year or month */) => {
   posts = posts.filter(p => get(p, 'node.frontmatter.published'))
   return groupBy(posts, p => {
-    return new Date(get(p, 'node.frontmatter.date')).getMonth()
+    const publishedDate = new Date(get(p, 'node.frontmatter.date'));
+    const groupName =  type === 'month' ? publishedDate.getMonth() : publishedDate.getFullYear();
+    return groupName;
   })
 }
 export default class IndexPage extends React.Component {
+  renderMonths(group, year) {
+    return (
+      Object.keys(group)
+      .reverse()
+      .map(month => {
+        let monthName = getMonthName(month)
+        let key = `${monthName}_${year}`;
+        let monthPosts = group[month]
+        return (
+          <div key={key} className="issues__preview-content">
+            {monthPosts.length > 0 && <div className="issues__month">{monthName} - {year}</div>}
+            {monthPosts.map(({ node }, index) => {
+              const title = get(node, 'frontmatter.title', node.fields.slug)
+              const published = get(node, 'frontmatter.published')
+              const contentTitle = get(node, 'frontmatter.contentTitle')
+              if (!published) return null
+              return (
+                <div className="issues__item" key={node.fields.slug}>
+                  <Link to={`/issues${node.fields.slug}`} className="issues__title">
+                    <div className="issues__info">
+                      <h3 className="issues_no">{title.replace('#', '')}</h3>
+                      <time className="issues__date">{node.frontmatter.date}</time>
+                    </div>
+                    <p>{contentTitle}</p>
+                  </Link>
+                </div>
+              )
+            })}
+          </div>
+        )
+      })
+    )
+  }
+
+  renderYears(groupedPosts) {
+    return (
+      Object.keys(groupedPosts).reverse().map(year => {
+        return this.renderMonths(groupedPosts[year], year);
+      })
+    )
+  }
+
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const siteDescription = get(this, 'props.data.site.siteMetadata.description')
     const posts = get(this, 'props.data.allMarkdownRemark.edges')
-    const groupedPosts = getGroupedPosts(posts)
+    const groupedPosts = getGroupedPosts(posts, 'year')
+
+    for(let year of Object.keys(groupedPosts)) {
+      const group = groupedPosts[year];
+      groupedPosts[year] = getGroupedPosts(group, 'month');
+    }
+
     return (
       <Layout>
         <SEO title={`${siteDescription}`} keywords={[siteDescription]} />
@@ -32,34 +82,7 @@ export default class IndexPage extends React.Component {
               <h2 className="title">All Issues</h2>
             </div>
             <div className="line" />
-            {Object.keys(groupedPosts)
-              .reverse()
-              .map(month => {
-                let monthName = getMonthName(month)
-                let monthPosts = groupedPosts[month]
-                return (
-                  <div key={monthName} className="issues__preview-content">
-                    {monthPosts.length > 0 && <div className="issues__month">{monthName}</div>}
-                    {monthPosts.map(({ node }, index) => {
-                      const title = get(node, 'frontmatter.title', node.fields.slug)
-                      const published = get(node, 'frontmatter.published')
-                      const contentTitle = get(node, 'frontmatter.contentTitle')
-                      if (!published) return null
-                      return (
-                        <div className="issues__item" key={node.fields.slug}>
-                          <Link to={`/issues${node.fields.slug}`} className="issues__title">
-                            <div className="issues__info">
-                              <h3 className="issues_no">{title.replace('#', '')}</h3>
-                              <time className="issues__date">{node.frontmatter.date}</time>
-                            </div>
-                            <p>{contentTitle}</p>
-                          </Link>
-                        </div>
-                      )
-                    })}
-                  </div>
-                )
-              })}
+            { this.renderYears(groupedPosts) } 
           </div>
         </ContentRight>
       </Layout>
